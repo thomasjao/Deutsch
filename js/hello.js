05ce55c1bcd547ec7a9c96527c6ad70986a53721
thomasjao <thomas.jao@gmail.com>
2019-10-01 23:17:05 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async(event, context) => {
     return {
         statusCode: 200,
-        body: "<h1>H1</h1><h2>H2 Header</h2><h3 style='color: steelblue'>H3 Header</h3><h4>Ignore</h4><p>ABC</p>"
+        body: "<h1>H1</h1><h2>H2 Header</h2><h3 style='color: steelblue'>H3 Header</h3><h4>Ignore</h4><p>ABC</p><form method='POST' action=''><label>Name: </label></form>"
     };
 }
