79686c3131573f4ead50028a0bdb369370d4eb7c
thomasjao <thomas.jao@gmail.com>
2019-10-01 23:06:31 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -2,6 +2,6 @@ exports.handler = async(event, context) => {
     return {
         myname: '饒裕奇',
         statusCode: 200,
-        body: "<form method='post' action=''><label>Name: <input type='text' name='name' placeholder='請輸入姓名' value='`${myname}`' required/></label><br /><label>Sex: <input type='radio' name='sex' value='male'/> 男<input type='radio' name='sex' value='female'/></label><br /><label>Age: <input type='text' name='age' value=''/></label><button>Press Me</button>"
+        body: "<form method='post' action=''><label>Name: <input type='text' name='name' placeholder='請輸入姓名' required/></label><br /><label>Sex: <input type='radio' name='sex' value='male'/> 男<input type='radio' name='sex' value='female'/></label><br /><label>Age: <input type='text' name='age' value=''/></label><button>Press Me</button>"
     };
 }
