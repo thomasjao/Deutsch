1bd69b11f1c7ba272f7bd01fe5db610b90b58f4a
thomasjao <thomas.jao@gmail.com>
2019-10-01 22:50:41 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async(event, context) => {
     return {
         statusCode: 200,
-        body: "<p>Take some HTML code here.</p>"
+        body: "<p style="color:red">Take some HTML code here.</p>"
     };
 }
