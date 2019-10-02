ae1b6e3c95cb7c36d2729ae8558de9ecb50b02a1
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:32:41 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,6 @@
 exports.handler = function(event, context) {
-  statusCode: 200,
-  body: ((var body=document.body;var arr=["DA1","DA2","DA3"];var s = arr.map((index, item) => { "item"+index: item}); body.append(s.toString())
+  return callback(null, {
+    statusCode: 200,
+    body: 'Let me do it'
+  });
 }
