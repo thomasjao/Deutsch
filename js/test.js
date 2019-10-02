258dc881c9526c99a14960eac54a65cd92c077f5
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:42:41 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
   return callback(null, {
     statusCode: 403,
     header: {'Content-type': 'text/plain'},
-    body: ([1,2,3,4].map((item) => 'ITEM ' + item)).join(', ').toString();)
+    body: ([1,2,3,4].map((item) => 'ITEM ' + item).join(', ').toString();)
   });
 }
