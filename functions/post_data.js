23990b03dcb5e10ea2187c99025ca22f44e5dce4
thomasjao <thomas.jao@gmail.com>
2019-10-02 11:21:31 +0800

    Create post_data.js

---
+++ functions/post_data.js
@@ -0,0 +1,7 @@
+exports.handler = function( event, context, callback) {
+  return callback(null, {
+    statusCode: 200,
+    headers: {'Content-Type': 'text/json'},
+    body: {"cities":[{"city": "Taipei","mayor":"柯文哲"},{"city":"New Taipei","mayor": "侯友宜"},{"city":"台中市","mayor": "盧秀燕"},{"city":"台南市","市長":"黃偉哲"},{"城市":"高雄市","市長":"韓國瑜"},{"城市":"桃園市","市長":"鄭文燦"}]}
+  });
+}
