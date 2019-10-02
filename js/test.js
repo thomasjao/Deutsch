243e293f04415038320d9f4a7f374b03fc6b1053
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:46:26 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-  var arrMerge = function() { return [1,2,3,4].map((item) => 'ITEM ' + item).join(', ').toString();
+  var arrMerge = function() { return [1,2,3,4].map((item) => 'ITEM ' + item).join(', ').toString();};
   return callback(null, {
     statusCode: 403,
     header: {'Content-type': 'text/plain'},
