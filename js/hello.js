2e5401da24b7cecf693ed2c4e48b9cbf6800957c
thomasjao <thomas.jao@gmail.com>
2019-10-02 16:24:47 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,6 @@
 exports.handler = async(event, context) => {
     return {
         statusCode: 200,
-        body: `Server connected successful with status code: ${statusCode}`
+        body: `Server connected successful with status code: ${this.statusCode}`
     };
 }
