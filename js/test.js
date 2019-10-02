84b153348c347fc0c2b631b8610efba7255b2308
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:30:26 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,1 +1,4 @@
-((var body=document.body;var arr=["DA1","DA2","DA3"];var s = arr.map((index, item) => { "item"+index: item}); body.append(s.toString());
+exports.handler = function(event, context) {
+  statusCode: 200,
+  body: ((var body=document.body;var arr=["DA1","DA2","DA3"];var s = arr.map((index, item) => { "item"+index: item}); body.append(s.toString())
+}
