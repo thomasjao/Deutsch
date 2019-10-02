a19d4ffe4a89db755d7f117d96eee8b3ca36e360
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:35:16 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
   return callback(null, {
     statusCode: 200,
     header: {'Content-type': 'text/plain'},
-    body: 'Let me do it'
+    body: '<a href="https://www.google.com/">Google</a>'
   });
 }
