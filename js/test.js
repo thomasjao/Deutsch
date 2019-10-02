7c3b98cdaf41cb113442188d3fa672f6c3914439
thomasjao <thomas.jao@gmail.com>
2019-10-02 21:34:26 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
   return callback(null, {
     statusCode: 200,
+    header: {'Content-type': 'text/plain'},
     body: 'Let me do it'
   });
 }
