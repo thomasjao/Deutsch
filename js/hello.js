5faf92ed1b172166e17243fae20c466c89b3a183
thomasjao <thomas.jao@gmail.com>
2019-10-02 16:28:31 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,7 @@
-exports.handler = async(event, context) => {
+exports.handler = async(event, {'Content-type': 'text/html') => {
+    var name = '饒裕奇';
     return {
         statusCode: 200,
-        body: `Server connected successful with status code: ${statusCode}`
+        body: `Server connected successful with status code: ${name}`
     };
 }
