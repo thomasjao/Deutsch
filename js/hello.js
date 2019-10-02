d5f1f0feea08c1468d5b35b416d0b26f5cc0c823
thomasjao <thomas.jao@gmail.com>
2019-10-02 16:30:21 +0800

    Update hello.js

--- js/hello.js
+++ js/hello.js
@@ -1,4 +1,4 @@
-exports.handler = async(event, {'Content-type': 'text/html'} => {
+exports.handler = async(event, {'Content-type': 'text/html'}) => {
     var name = '饒裕奇';
     return {
         statusCode: 200,
